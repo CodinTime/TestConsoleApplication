cf5b4d0e151eb5bb7ba96702792cc34722770394
Krzysztof Konobrocki <krzysk46@gmail.com>
2017-06-09 08:51:51 +0200

    Added classes PersonDto, PersonViewModel

--- TestConsoleApplication/Program.cs
+++ TestConsoleApplication/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using AutoMapper;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +11,7 @@ namespace TestConsoleApplication
     {
         static void Main(string[] args)
         {
+            var config = new MapperConfiguration(cfg => cfg.CreateMap<,>());
         }
     }
 }
